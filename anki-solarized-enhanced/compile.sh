e7be8c6ab500387f3354139862e70cfeb6281968
Jan Schmitz <Janschmitz99@gmail.com>
2023-01-04 23:57:08 +0100

    add generic verb card type

--- anki-solarized-enhanced/compile.sh
+++ anki-solarized-enhanced/compile.sh
@@ -3,3 +3,5 @@
 echo "Compiling generic-noun.scss"
 sass generic-noun/generic-noun.scss generic-noun/generic-noun.css
 
+echo "Compiling generic-verb.scss"
+sass generic-verb/generic-verb.scss generic-verb/generic-verb.css
